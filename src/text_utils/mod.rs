b10daca8aea51f2ec0d1df9f204e93545000a6c0
Filip Tibell <filip.tibell@gmail.com>
2025-06-19 23:56:40 +0200

    Add text utils for common operations for byte ranges

--- src/text_utils/mod.rs
+++ src/text_utils/mod.rs
@@ -2,6 +2,8 @@ mod conversions;
 mod encoding;
 mod position;
 
+pub mod byte_range;
+
 pub use self::conversions::position_to_encoding;
 pub use self::encoding::Encoding;
 pub use self::position::Position;
