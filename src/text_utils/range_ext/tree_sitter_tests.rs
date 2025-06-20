22b3be3b435bdd43b6cdf8f3f00aa56d94cae869
Filip Tibell <filip.tibell@gmail.com>
2025-06-20 13:24:55 +0200

    Scaffold for tree sitter suite

--- src/text_utils/range_ext/tree_sitter_tests.rs
+++ src/text_utils/range_ext/tree_sitter_tests.rs
@@ -1,1 +1,27 @@
+use tree_sitter::{Point as TsPosition, Range as TsRange};
 
+use super::RangeExt;
+
+const LF: char = '\n';
+const D1: char = '/';
+const D2: char = '@';
+
+const fn r(
+    start_byte: usize,
+    start_position: TsPosition,
+    end_byte: usize,
+    end_position: TsPosition,
+) -> TsRange {
+    TsRange {
+        start_byte,
+        start_point: start_position,
+        end_byte,
+        end_point: end_position,
+    }
+}
+
+const fn p(line: usize, column: usize) -> TsPosition {
+    TsPosition { row: line, column }
+}
+
+// Tests go here ...
